b48b64b0b6bed2e18b537c6095e7486342d608f6
anthonyerwin <103916756+anthonyerwin@users.noreply.github.com>
2022-04-22 18:53:19 +0800

    Second Bowling Commit
    raw files

---
+++ src/Bowler.java
@@ -0,0 +1,34 @@
+public class Bowler {
+    private int pins;
+    private int score;
+    private int totalScore;
+
+    public Bowler() {
+
+    }
+
+    public int getPins() {
+        return pins;
+    }
+
+    public void setPins(int pins) {
+        this.pins = pins;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+}
